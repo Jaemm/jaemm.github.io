e02027113ba3f81ee49033feb3db83da3a9324aa
Jaemin Choi <jmchoi@chowis.com>
2026-06-23 16:21:01 +0900

    Clean up lint warnings

--- src/components/ui/svgs/tech/aws.tsx
+++ src/components/ui/svgs/tech/aws.tsx
@@ -1,6 +1,8 @@
 import type { ImgHTMLAttributes } from "react";
 
 const Aws = (props: ImgHTMLAttributes<HTMLImageElement>) => (
+  // External favicon; keep a plain image element and silence the Next.js lint rule.
+  // eslint-disable-next-line @next/next/no-img-element
   <img
     {...props}
     src="https://aws.amazon.com/favicon.ico"
--- src/components/ui/svgs/tech/nestjs.tsx
+++ src/components/ui/svgs/tech/nestjs.tsx
@@ -1,6 +1,8 @@
 import type { ImgHTMLAttributes } from "react";
 
 const Nestjs = (props: ImgHTMLAttributes<HTMLImageElement>) => (
+  // External logo asset; keep a plain image element and silence the Next.js lint rule.
+  // eslint-disable-next-line @next/next/no-img-element
   <img
     {...props}
     src="https://nestjs.com/3d.cfabe065.svg"
